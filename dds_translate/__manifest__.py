80aba7de1989091e70f2d46d627013bf33c4d35e
robertsvajdlenka <robert.svajdlenka@dodosys.sk>
2023-12-12 14:05:09 +0100

    tra

---
+++ dds_translate/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "DDS - Translate",
+    'author': "Dodo Systems s. r. o.",
+    'website': "https://www.dodosys.sk/",
+    'category': 'Translations',
+    'version': '16.0.1.0',
+    'license': 'OPL-1',
+    'depends': [],
+    'installable': True,    
+    'application': False,
+    'auto_install': False
+}
---
+++ dds_translate_account_fleet/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "DDS - Translate - account_fleet",
+    'author': "Dodo Systems s. r. o.",
+    'website': "https://www.dodosys.sk/",
+    'category': 'Translations',
+    'version': '16.0.1.1',
+    'license': 'OPL-1',
+    'depends': ['account_fleet', 'dds_translate'],
+    'installable': True,    
+    'application': False,
+    'auto_install': True
+}
